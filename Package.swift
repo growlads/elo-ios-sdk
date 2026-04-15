e76004fb0d0aea3697d6ea1840ba117da0608cb3
Gökhan Namal <gokhanamal@gmail.com>
2026-04-15 09:30:06 +0200

    Release 0.0.1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GrowlAds",
-            url: "https://github.com/growlads/growl-ios-sdk/releases/download/1.0.0/GrowlAds.xcframework.zip",
-            checksum: "CHECKSUM_PLACEHOLDER"
+            url: "https://github.com/growlads/growl-ios-sdk/releases/download/0.0.1/GrowlAds.xcframework.zip",
+            checksum: "b547fdc54fc31e7bd1083d7a450aae75b98e0a91ed9d7417e2de812111a5c6a9"
         ),
     ]
 )
